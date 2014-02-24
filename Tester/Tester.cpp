4f6ff20bac924634d6ae3f22b39e4c5fb1779037
Aloalo <ratkovic.jure@gmail.com>
2014-02-24 15:02:38 +0100

    downgfr

--- Tester/Tester.cpp
+++ Tester/Tester.cpp
@@ -111,13 +111,13 @@ int main(int argc, char* argv[])
 	
 	//addSineCosine();
 	//addSphere();
-	addObjFile("../Resources/sponza/sponza.obj", "../Resources/sponza/");
+	//addObjFile("../Resources/sponza/sponza.obj", "../Resources/sponza/");
 	//addObjFile("../Resources/nissan/nissan.obj", "../Resources/nissan/", scale(mat4(1.0f), vec3(0.05f, 0.05f, 0.05f)));
 	//addObjFile("../Resources/demon/demon1_OBJ.obj", "../Resources/demon/", translate(mat4(1.0f), vec3(-10, 0, 0)));
 	
-	//addObjFile("../Resources/sibenik/sibenik.obj", "../Resources/sibenik/");
+	addObjFile("../Resources/sibenik/sibenik.obj", "../Resources/sibenik/");
 	//addObjFile("../Resources/dragon/dragon.obj", "../Resources/dragon/", scale(mat4(1.0f), vec3(3.1f, 3.1f, 3.1f)));
-	//addObjFile("../Resources/rungholt/rungholt.obj", "../Resources/rungholt/", scale(mat4(1.0f), vec3(0.01f, 0.01f, 0.01f)));
+	//addObjFile("../Resources/rungholt/rungholt.obj", "../Resources/rungholt/", scale(mat4(1.0f), vec3(0.1f, 0.1f, 0.1f)));
 	//addObjFile("../Resources/head/head.obj", "../Resources/head/");
 	//addObjFile("../Resources/powerplant/powerplant.obj", "../Resources/powerplant/", scale(mat4(1.0f), vec3(0.1f, 0.1f, 0.1f)));
 	//addObjFile("../Resources/buddha/buddha.obj", "../Resources/buddha/", scale(mat4(1.0f), vec3(3.1f, 3.1f, 3.1f)));
