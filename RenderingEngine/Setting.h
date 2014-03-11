1b7920f0906b5432e743b61451655fc3022aa40b
Aloalo <ratkovic.jure@gmail.com>
2014-03-11 18:03:02 +0100

    settings

---
+++ RenderingEngine/Setting.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "InitialSettings.h"
+
+#define SETTING(s) s(#s)
+
+namespace reng
+{
+	template<class T>
+	struct Setting
+	{
+		Setting(const std::string &name) :
+			name(name)
+		{
+			x = InitialSettings::get()[name];
+		}
+
+
+		//void set(const map<std::string, T> &mp)
+		//{
+		//	x = mp[name];
+		//}
+
+		operator T&()
+		{
+			return x;
+		}
+
+		T& operator=(const T &v)
+		{
+			return x = v;
+		}
+
+		T x;
+		std::string name;
+	};
+}
